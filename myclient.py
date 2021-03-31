3ec2364f77147e54317c49f91c93a7f9df78d861
Staisy <Canada_19@mail.ru>
2021-03-31 23:45:53 +0300

    новые допы

--- myclient.py
+++ myclient.py
@@ -1,32 +1,47 @@
 import socket
 
 sock = socket.socket()
+sock.setblocking(1)
 
-address = 'localhost'
-port = input('Input port: ')
-#ort = 9000
-if not port:
-    port = 9000
-else:
-    port = int(port)
+#address = 'localhost'
+while True:
+    try:
+        port = int(input('Input port: '))
+        address = input('Input hostname: ')
+        sock.connect((address, port))
+        print('Server connection', file=open('config.txt', 'a'))
+        break
+    except socket.gaierror:
+        address = '192.168.68.27'
+        port = 9064
+        print('Incorrect data, open socket with port = ', port, ', address - ', address)
+        try:
+            sock.connect((address, port))
+            break
+        except OSError:
+            port += 1
+    except ConnectionRefusedError:
+        print('Smth went wrong, try again')
 
-sock.setblocking(True)
-sock.connect((address, port))
-print('Server connection')
+
+data = sock.recv(1024).decode()
+if 'Enter your nickname:' in data:
+    print(data)
+    name = input('Nickname: ')
+    sock.send(name.encode())
+    print(sock.recv(1024).decode())
+
 
 while True:
     sent = input('Input sentence: ')
     if sent == 'exit':
         sock.close()
-        print('Disconnection')
+        print('Disconnection', file=open('config.txt', 'a'))
         break
-        
     sock.send(sent.encode())
+    print('Sending data', file=open('config.txt', 'a'))
     data = sock.recv(1024)
+    print('Receiving data', file=open('config.txt', 'a'))
     print(data.decode())
-    print('Receiving data')
-
-print('Sending data')
-
-
+print('Disconnection', file=open('config.txt', 'a'))
 
--- myserver.py
+++ myserver.py
@@ -1,29 +1,51 @@
+import pickle
 import socket
 
+print('Server start')
 sock = socket.socket()
-port = 9000
+port = 9064
+
 while True:
     try:
         sock.bind(('', port))
+        print('Port connected', port)
         break
     except OSError:
         port += 1
 
-print('Server start')       
-sock.listen(1)
-print('Port connected')
+sock.listen(0)
+sent = ''
 
+f = open('users', 'w')
+users = open("users", "rb")
+try:
+    users = pickle.load(users)
+except EOFError:
+    user = {}
 
 while True:
     print('Client connection')
     conn, addr = sock.accept()
     print('connected:', addr)
-    while True:
+    username = user.get(addr[0])
+    if username:
+        conn.send(f'User -  {username},  welcome back'.encode())
+    else:
+        conn.send(f'Enter your nickname: '.encode())
         data = conn.recv(1024)
+        user.setdefault(addr[0], data.decode())
+        users = open("users", "wb")
+        pickle.dump(user, users)
+        users.close()
+        conn.send(f'Hi, {user.get(addr[0])}'.encode())
+    while True:
         print('Receiving data')
+        data = conn.recv(1024)
+        print(data)
         if not data:
            break
+        sent += data.decode()
         print('Sending data')
         conn.send(data)
-        print(data.decode())
-
+    print(sent)
+    conn.close()
